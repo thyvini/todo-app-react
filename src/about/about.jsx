a8b1c629d12dbbff14a55d8fb0a75656b54fcfa7
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2019-09-24 01:20:57 -0300

    First Commit

---
+++ src/about/about.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PageHeader from '../template/pageheader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto consectetur, cupiditate dolorem, ea
+            est expedita incidunt iusto magnam odit officia, perspiciatis qui ratione recusandae repellendus tempore
+            tenetur vel voluptates.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis ipsum minima modi sed velit? Ad at beatae
+            culpa earum libero magnam mollitia nesciunt non ratione, sint sit ullam, voluptas voluptatibus!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi blanditiis, commodi deleniti deserunt
+            dicta doloremque, dolorum est expedita id impedit laudantium maxime modi molestias nesciunt, quae saepe
+            tempore voluptatibus!</p>
+    </div>
+);
---
+++ src/todo/todoList.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import IconButton from '../template/iconButton';
+
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || [];
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+                <td className='tableActions'>
+                    <IconButton
+                        style='success'
+                        icon='check'
+                        onClick={() => props.handleMarkAsDone(todo)}
+                        hide={todo.done}
+                    />
+                    <IconButton
+                        style='warning'
+                        icon='undo'
+                        onClick={() => props.handleMarkAsPending(todo)}
+                        hide={!todo.done}
+                    />
+                    <IconButton
+                        style='danger'
+                        icon='trash-o'
+                        onClick={() => props.handleRemove(todo)}
+                    />
+                </td>
+            </tr>
+        ));
+    };
+
+    return (
+        <table className='table'>
+            <thead>
+            <tr>
+                <th>Descrição</th>
+                <th>Ações</th>
+            </tr>
+            </thead>
+            <tbody>
+            {renderRows()}
+            </tbody>
+        </table>
+    );
+}
